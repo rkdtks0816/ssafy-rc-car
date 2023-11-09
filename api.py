1dc79dc12509f3b33fdf32b7ac65b285877888f3
rkdtks0816 <rkdtks0816@naver.com>
2023-11-09 17:52:06 +0900

    2

--- api.py
+++ api.py
@@ -27,11 +27,14 @@ async def send_camera_image(websocket):
 
     while True:
         ret, frame = cap.read()
-        frame = cv2.flip(frame, -1)
+        (h, w) = frame.shape[:2]
+        (cX, cY) = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D((cX, cY), -90, 1.0)
+        rotated_90 = cv2.warpAffine(frame, M, (w, h))
         if not ret:
             continue
 
-        _, buffer = cv2.imencode('.jpg', frame)
+        _, buffer = cv2.imencode('.jpg', rotated_90)
         base64_image = base64.b64encode(buffer).decode('utf-8')
 
         await websocket.send_text(base64_image)
